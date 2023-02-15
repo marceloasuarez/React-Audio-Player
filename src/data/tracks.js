87796b097a79fc1885e6a6eead9c9c6edfe437bd
MARCELO ADRIAN SUAREZ <90062398+marceloasuarez@users.noreply.github.com>
2023-02-15 17:57:32 -0300

    React Audio Player

---
+++ src/data/tracks.js
@@ -0,0 +1,23 @@
+// audio files
+import beautiful from './If-I-Had-a-Chicken.mp3';
+import world from './Malt-Shop-Bop.mp3';
+
+// audio thumbnails
+import jackson from './kevinmacleod-1.jpg';
+import trinix from './kevinmacleod-2.jpg';
+
+export const tracks = [
+  {
+    title: 'If-I-Had-a-Chicken',
+    src: beautiful,
+    author: 'Kevin Mac Leod',
+    thumbnail: trinix,
+  },
+  {
+    title: 'Malt-Shop-Bop',
+    src: world,
+    author: 'Kevin Mac Leod',
+    thumbnail: jackson,
+  },
+  
+];
